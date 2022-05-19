b8b069b7268a26c70f39d3d01a3a97996bf2ebb6
ArmaanTechDeveloper <kournarinder948@gmail.com>
2022-05-19 14:41:17 +0530

    awesome pattern questions 4.1

---
+++ DSA/rectanglePattern.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int rows,columns;
+    cout<<"Enter the number of rows & columns ";
+    cin>>rows>>columns;
+    cout<<endl;
+    for(int i = 1 ; i <= rows ; i++){
+        for(int j = 1 ; j <= columns ; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
